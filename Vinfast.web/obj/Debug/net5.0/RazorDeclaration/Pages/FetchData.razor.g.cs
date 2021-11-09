2eda88f010fa3f31b1cd7ab7ec3b070ebcb43cd8
Admin-PC\Admin <minhho1220@gmail.com>
2021-11-09 18:38:26 +0700

    mh

--- Vinfast.web/obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
+++ Vinfast.web/obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
@@ -13,77 +13,77 @@ namespace Vinfast.web.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\Vinfast\Vinfast.web\_Imports.razor"
+#line 1 "D:\DuAnVinFast\Vinfast.web\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\Vinfast\Vinfast.web\_Imports.razor"
+#line 2 "D:\DuAnVinFast\Vinfast.web\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\Vinfast\Vinfast.web\_Imports.razor"
+#line 3 "D:\DuAnVinFast\Vinfast.web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\Vinfast\Vinfast.web\_Imports.razor"
+#line 4 "D:\DuAnVinFast\Vinfast.web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\Vinfast\Vinfast.web\_Imports.razor"
+#line 5 "D:\DuAnVinFast\Vinfast.web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\Vinfast\Vinfast.web\_Imports.razor"
+#line 6 "D:\DuAnVinFast\Vinfast.web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\Vinfast\Vinfast.web\_Imports.razor"
+#line 7 "D:\DuAnVinFast\Vinfast.web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\Vinfast\Vinfast.web\_Imports.razor"
+#line 8 "D:\DuAnVinFast\Vinfast.web\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\Vinfast\Vinfast.web\_Imports.razor"
+#line 9 "D:\DuAnVinFast\Vinfast.web\_Imports.razor"
 using Vinfast.web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "D:\Vinfast\Vinfast.web\_Imports.razor"
+#line 10 "D:\DuAnVinFast\Vinfast.web\_Imports.razor"
 using Vinfast.web.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\Vinfast\Vinfast.web\Pages\FetchData.razor"
+#line 3 "D:\DuAnVinFast\Vinfast.web\Pages\FetchData.razor"
 using Vinfast.web.Data;
 
 #line default
@@ -98,7 +98,7 @@ using Vinfast.web.Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 39 "D:\Vinfast\Vinfast.web\Pages\FetchData.razor"
+#line 39 "D:\DuAnVinFast\Vinfast.web\Pages\FetchData.razor"
        
     private WeatherForecast[] forecasts;
 
