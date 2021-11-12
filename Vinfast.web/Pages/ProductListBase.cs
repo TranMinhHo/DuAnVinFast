72fd089436a5e494d1a934c6dc44d7477d4dc30e
Admin-PC\Admin <minhho1220@gmail.com>
2021-11-12 19:56:43 +0700

    ImageEvent

--- Vinfast.web/Pages/ProductListBase.cs
+++ Vinfast.web/Pages/ProductListBase.cs
@@ -11,6 +11,9 @@ namespace Vinfast.web.Pages
 {
     public class ProductListBase : ComponentBase
     {
+        public string Ten { get; set; }
+        public string Datcoc { get; set; }
+        public string ImageOnClick1 { get; set; }
         [Inject]
         public IProduceService ProduceService { get; set; }
         public IEnumerable<Product> Products { get; set; }
@@ -38,6 +41,36 @@ namespace Vinfast.web.Pages
             {
                 SelectedProductCount--;
             }
+        }
+      
+        public async Task Img_Fadil()
+        {
+            Products = (await ProduceService.GetProducts()).ToList();
+            ImageOnClick1 = "/image/fadil360.png";
+            Datcoc = "30.000.000";
+            Ten = "FADIL";
+             
+        }
+        public async Task Img_Sa()
+        {
+            Products = (await ProduceService.GetProducts()).ToList();
+            ImageOnClick1 = "/image/sa360.png";
+            Datcoc = "50.000.000";
+            Ten = "SA2.0";
+        }
+        public async Task Img_President()
+        {
+            Products = (await ProduceService.GetProducts()).ToList();
+            ImageOnClick1 = "/image/president360.png";
+            Datcoc = "100.000.000";
+            Ten = "PRESIDENT";
+        }
+        public async Task Img_A20()
+        {
+            Products = (await ProduceService.GetProducts()).ToList();
+            ImageOnClick1 = "/image/a360.png";
+            Datcoc = "50.000.000";
+            Ten = "A20";
         }
     }
  }
